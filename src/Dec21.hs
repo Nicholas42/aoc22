41ed874238720acfed03aabf7e1133f0f550ed49
Nicholas Schwab <nicholas.schwab@tngtech.com>
2022-12-22 13:46:15 +0100

    Add solution for day 22.

--- src/Dec21.hs
+++ src/Dec21.hs
@@ -1,8 +1,160 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Dec21
   ( run
   ) where
 
+import Control.Applicative ((*>), (<*), (<|>), liftA)
+import Control.Monad.State
+import Data.Attoparsec.Combinator ((<?>))
+import qualified Data.Attoparsec.Text as AP
+import Data.Function (on)
+import qualified Data.Map as M
+import Data.Map ((!))
+import qualified Data.Text as T
+import Debug.Trace
+
+data Monkey
+  = Leaf
+      { name :: String
+      , value :: Integer
+      }
+  | Node
+      { name :: String
+      , lhs :: String
+      , op :: Char
+      , rhs :: String
+      }
+  deriving (Show)
+
+parseNumberMonkey :: AP.Parser Monkey
+parseNumberMonkey = do
+  num <- AP.decimal
+  return $ Leaf {name = "", value = num}
+
+parseOpMonkey :: AP.Parser Monkey
+parseOpMonkey = do
+  lhs <- word <* " "
+  op <- AP.anyChar
+  rhs <- " " *> word
+  return $ Node {name = "", lhs = lhs, op = op, rhs = rhs}
+
+word :: AP.Parser String
+word = AP.many1 AP.letter
+
+parseMonkey :: AP.Parser (String, Monkey)
+parseMonkey = do
+  name <- word <* ": "
+  next <- AP.peekChar
+  monkey <- (parseOpMonkey <|> parseNumberMonkey) <* AP.endOfLine
+  return $ (name, monkey {name = name})
+
+parseAllMonkeys :: T.Text -> M.Map String Monkey
+parseAllMonkeys text =
+  case AP.parseOnly ((AP.many1 parseMonkey) <* AP.endOfInput) text of
+    Left a -> error a
+    Right b -> M.fromList b
+
+getOp :: Char -> (Integer -> Integer -> Integer)
+getOp '+' = (+)
+getOp '-' = (-)
+getOp '*' = (*)
+getOp '/' = div
+
+evalMonkey :: M.Map String Monkey -> String -> Integer
+evalMonkey mappy name = do
+  let monkey = mappy ! name
+  case monkey of
+    Leaf {value = a} -> a
+    Node {lhs = lhs, op = op, rhs = rhs} ->
+      ((getOp op) `on` (evalMonkey mappy)) lhs rhs
+
+tryNumber :: M.Map String Monkey -> Integer -> Bool
+tryNumber mappy human = do
+  let newMap = M.insert "humn" (Leaf {name = "humn", value = human}) mappy
+  0 == evalMonkey newMap "root"
+
+dependsOn :: M.Map String Monkey -> String -> String -> Bool
+dependsOn mappy target source = do
+  if target == source
+    then True
+    else case mappy ! source of
+           Leaf {} -> False
+           Node {lhs = lhs, rhs = rhs} ->
+             any (dependsOn mappy target) [lhs, rhs]
+
+simplifyOp :: Monkey -> Monkey -> Monkey -> Monkey
+simplifyOp opMonkey lhs@(Leaf {}) rhs@(Leaf {})
+  | name lhs == "humn" || name rhs == "humn" =
+    opMonkey {lhs = name lhs, rhs = name rhs}
+  | otherwise =
+    Leaf
+      {name = name opMonkey, value = ((getOp $ op opMonkey) `on` value) lhs rhs}
+simplifyOp opMonkey _ _ = opMonkey
+
+simplifyNode :: Monkey -> State (M.Map String Monkey) Monkey
+simplifyNode m@(Leaf {}) = return m
+simplifyNode opMonkey = do
+  newLhs <- simplify $ lhs opMonkey
+  newRhs <- simplify $ rhs opMonkey
+  let result = (simplifyOp opMonkey newLhs newRhs)
+  modify $ (\m -> M.insert (name opMonkey) result m)
+  return $ result
+
+simplify :: String -> State (M.Map String Monkey) Monkey
+simplify name = do
+  mappy <- get
+  simplifyNode $ mappy ! name
+
+runSimplification :: M.Map String Monkey -> M.Map String Monkey
+runSimplification mappy = execState (simplify "root") mappy
+
+maybeValue :: Monkey -> Maybe Integer
+maybeValue Leaf{name="humn"} = Nothing
+maybeValue Leaf{value=v} = Just v
+maybeValue _ = Nothing
+
+revOp :: Integer -> Maybe Integer -> Char -> Maybe Integer -> Integer
+revOp result Nothing '+' (Just r) = result - r
+revOp result (Just l) '+' Nothing = result - l
+revOp result Nothing '-' (Just r) = result + r
+revOp result (Just l) '-' Nothing = l - result
+revOp result Nothing '*' (Just r) = result `div` r
+revOp result (Just l) '*' Nothing = result `div` l
+revOp result Nothing '/' (Just r) = result * r
+revOp result (Just l) '/' Nothing = l `div` result
+
+index :: Ord k => k -> M.Map k v -> v
+index key mappy = mappy ! key
+
+findNeededOp ::  M.Map String Monkey -> Monkey -> Integer -> Maybe Integer 
+findNeededOp mappy monkey result = do
+    let lhsMonkey = maybeValue $ mappy ! lhs monkey
+    let rhsMonkey = maybeValue $ mappy ! rhs monkey
+    let neededResult = revOp result lhsMonkey (op monkey) rhsMonkey
+    case lhsMonkey of
+        Nothing -> findNeeded mappy (lhs monkey) neededResult
+        _ -> findNeeded mappy (rhs monkey) neededResult
+
+
+findNeeded :: M.Map String Monkey -> String -> Integer -> Maybe Integer
+findNeeded _ "humn" result = Just result
+findNeeded mappy name result = do
+    case mappy ! name of
+        Leaf {} -> Nothing
+        m@Node {} -> findNeededOp mappy m result
+
+
 run :: IO ()
 run = do
-  input <- readFile "inputs/dec21.txt"
-  print input
+  input <- parseAllMonkeys <$> T.pack <$> readFile "inputs/dec21.txt"
+  print $ evalMonkey input "root"
+  let root = input ! "root"
+  print $ dependsOn input "humn" $ lhs root
+  print $ dependsOn input "humn" $ rhs root
+  let simplified = (M.adjust (\m -> m{op='-'}) "root" $ runSimplification input)
+  print $ simplified ! "root"
+  print $ simplified ! rhs (simplified ! lhs (simplified ! "root"))
+  print $ findNeeded simplified "root" 0
+  -- let input2 = M.adjust (\m -> m {op = '-'}) "root" input 
+  -- print $ head $ filter (tryNumber input2) [0..50000]
--- src/Dec22.hs
+++ src/Dec22.hs
@@ -2,7 +2,196 @@ module Dec22
   ( run
   ) where
 
+import Control.Monad.State
+import Data.Char (isDigit)
+import qualified Data.List as L
+import qualified Data.Map as M
+import Data.Map ((!))
+import Data.Maybe (fromJust, isJust)
+import Debug.Trace
+
+data Position =
+  Position
+    { x :: Int
+    , y :: Int
+    }
+  deriving (Show, Ord, Eq)
+
+data Direction
+  = North
+  | East
+  | South
+  | West
+  deriving (Show)
+
+data Instruction
+  = Go Int
+  | TurnLeft
+  | TurnRight
+  deriving (Show)
+
+data Field
+  = Wall
+  | Free
+  deriving (Show, Eq)
+
+data GameState =
+  GameState
+    { gsMap :: M.Map Position Field
+    , gsFacing :: Direction
+    , gsPosition :: Position
+    }
+
+readField :: Char -> Maybe Field
+readField '.' = Just Free
+readField '#' = Just Wall
+readField ' ' = Nothing
+
+readInstruction :: String -> Instruction
+readInstruction "L" = TurnLeft
+readInstruction "R" = TurnRight
+readInstruction x = Go $ read x
+
+readInstructions :: String -> [Instruction]
+readInstructions line =
+  map readInstruction $ L.groupBy (\a -> \b -> isDigit a == isDigit b) line
+
+readMapLine :: Int -> String -> M.Map Position Field
+readMapLine y line =
+  M.fromList
+    [ (Position {x = x, y = y}, fromJust field)
+    | (x, field) <- zip [1 ..] $ map readField line
+    , isJust field
+    ]
+
+readMap :: [String] -> M.Map Position Field
+readMap lines = M.unions $ map (uncurry readMapLine) $ zip [1 ..] lines
+
+findStart :: M.Map Position Field -> Position
+findStart mappy = minimum $ filter (\p -> y p == 1) $ M.keys mappy
+
+parseInput :: String -> (GameState, [Instruction])
+parseInput text = do
+  let (mapInput, instructionInput) = L.break (== "") $ lines text
+  let mappy = readMap mapInput
+  (,) GameState {gsMap = mappy, gsFacing = East, gsPosition = findStart mappy} $
+    readInstructions $ instructionInput !! 1
+
+turnRight :: Direction -> Direction
+turnRight dir =
+  case dir of
+    East -> South
+    South -> West
+    West -> North
+    North -> East
+
+turnLeft dir =
+  case dir of
+    East -> North
+    North -> West
+    West -> South
+    South -> East
+
+stepTo :: Position -> Direction -> Position
+stepTo (Position x y) East = Position (x + 1) y
+stepTo (Position x y) South = Position x (y + 1)
+stepTo (Position x y) West = Position (x - 1) y
+stepTo (Position x y) North = Position x (y - 1)
+
+findWrap :: Position -> Direction -> M.Map Position Field -> Position
+findWrap pos dir mappy =
+  case dir of
+    East -> minimum $ filter (\p -> y p == y pos) $ M.keys mappy
+    South -> minimum $ filter (\p -> x p == x pos) $ M.keys mappy
+    West -> maximum $ filter (\p -> y p == y pos) $ M.keys mappy
+    North -> maximum $ filter (\p -> x p == x pos) $ M.keys mappy
+
+findWrapCube :: Position -> Direction -> (Position, Direction)
+findWrapCube pos North
+  | x pos <= 50 = (Position {x = 51, y = 50 + x pos}, East)
+  | x pos <= 100 = (Position {x = 1, y = 100 + x pos}, East)
+  | otherwise = (Position {x = (x pos) - 100, y = 200}, North)
+findWrapCube pos South
+  | x pos <= 50 = (Position {x = (x pos) + 100, y = 1}, South)
+  | x pos <= 100 = (Position {x = 50, y = 100 + (x pos)}, West)
+  | otherwise = (Position {x = 100, y = (x pos) - 50}, West)
+findWrapCube pos East
+  | y pos <= 50 = (Position {x = 100, y = 151 - (y pos)}, West)
+  | y pos <= 100 = (Position {x = 50 + (y pos), y = 50}, North)
+  | y pos <= 150 = (Position {x = 150, y = 151 - (y pos)}, West)
+  | otherwise = (Position {x = (y pos) - 100, y = 150}, North)
+findWrapCube pos West
+  | y pos <= 50 = (Position {x = 1, y = 151 - (y pos)}, East)
+  | y pos <= 100 = (Position {x = (y pos) - 50, y = 101}, South)
+  | y pos <= 150 = (Position {x = 51, y = 151 - (y pos)}, East)
+  | otherwise = (Position {x = (y pos) - 100, y = 1}, South)
+
+makeStep :: State GameState ()
+makeStep = do
+  GameState {gsMap = mappy, gsPosition = pos, gsFacing = dir} <- get
+  let stepped = stepTo pos dir
+  let nextPos =
+        if M.member stepped mappy
+          then stepped
+          else findWrap pos dir mappy
+  if mappy ! nextPos == Free
+    then modify $ (\gs -> gs {gsPosition = nextPos})
+    else return ()
+
+makeCubeStep :: State GameState ()
+makeCubeStep = do
+  GameState {gsMap = mappy, gsPosition = pos, gsFacing = dir} <- get
+  let stepped = stepTo pos dir
+  let (nextPos, nextDir) =
+        if M.member stepped mappy
+          then (stepped, dir)
+          else findWrapCube pos dir
+  if mappy ! nextPos == Free
+    then modify $ (\gs -> gs {gsPosition = nextPos, gsFacing = nextDir})
+    else return ()
+
+executeInstruction :: Instruction -> State GameState ()
+executeInstruction TurnLeft =
+  modify $ \gs -> gs {gsFacing = turnLeft $ gsFacing gs}
+executeInstruction TurnRight =
+  modify $ \gs -> gs {gsFacing = turnRight $ gsFacing gs}
+executeInstruction (Go steps) = do
+  sequence_ $ replicate steps makeStep
+
+executeInstructions :: GameState -> [Instruction] -> GameState
+executeInstructions gs instructions =
+  execState (mapM_ executeInstruction instructions) gs
+
+executeInstructionOnCube :: Instruction -> State GameState ()
+executeInstructionOnCube TurnLeft =
+  modify $ \gs -> gs {gsFacing = turnLeft $ gsFacing gs}
+executeInstructionOnCube TurnRight =
+  modify $ \gs -> gs {gsFacing = turnRight $ gsFacing gs}
+executeInstructionOnCube (Go steps) = do
+  sequence_ $ replicate steps makeCubeStep
+  foo <- get
+  return ()
+
+executeInstructionsOnCube :: GameState -> [Instruction] -> GameState
+executeInstructionsOnCube gs instructions =
+  execState (mapM_ executeInstructionOnCube instructions) gs
+
+dirValue :: Direction -> Int
+dirValue dir =
+  case dir of
+    East -> 0
+    South -> 1
+    West -> 2
+    North -> 3
+
+calcValue :: GameState -> Int
+calcValue (GameState {gsPosition = pos, gsFacing = dir}) =
+  1000 * (y pos) + 4 * (x pos) + dirValue dir
+
 run :: IO ()
 run = do
-  input <- readFile "inputs/dec22.txt"
-  print input
+  (gameState, instructions) <- parseInput <$> readFile "inputs/dec22.txt"
+  let finalGS = executeInstructions gameState instructions
+  print $ calcValue finalGS
+  let finalCube = executeInstructionsOnCube gameState instructions
+  print $ calcValue finalCube
